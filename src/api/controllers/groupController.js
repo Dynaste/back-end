8543d738a8c6173ef85f7a834f4926fda09bb3b0
Dynaste <maximemary.pro@gmail.com>
2020-11-26 10:39:30 +0100

    correctif verification group inscription -- all works

--- src/api/controllers/groupController.js
+++ src/api/controllers/groupController.js
@@ -1,5 +1,9 @@
 const Group = require("../models/groupModel");
-const {capitalize, checkMembersKeys, checkQuestionsKeys} = require('../utils/utils');
+const {
+  capitalize,
+  checkMembersKeys,
+  checkQuestionsKeys,
+} = require("../utils/utils");
 
 // #region Groups
 
@@ -28,51 +32,74 @@ exports.list_all_groups = (req, res) => {
  * @param {*} req The request sent, where req.body will contains all data we'll need to do the post.
  * @param {*} res The response of the request.
  */
-exports.create_a_group = async(req, res) => {
-  const {members} = req.body;
-  const {questions} = req.body;
-  const {about} = req.body;
+exports.create_a_group = async (req, res) => {
+  const { members, questions, about } = req.body;
 
   try {
-
-    /*
-      let verifDuplicate = await Group.find({members}).exec(); 
-      console.log(verifDuplicate);  // get tout les groupes afin de pouvoir ensuite boucler dessus pour verifier si des mails/phone existe deja
-    */
-
     if (checkMembersKeys(members) && checkQuestionsKeys(questions) && about) {
+      const emailDoublons = [];
 
-      
-      members.forEach(member => {
+      /**
+       * Format members property to avoid non capitalize firstname, lower-cased email and upper-cased lastname.
+       */
+      members.forEach((member) => {
         member.lastname = member.lastname.toUpperCase();
         member.firstname = capitalize(member.firstname);
         member.email = member.email.toLowerCase();
-      })
 
-      const new_group = new Group({ ...req.body });
-
-      new_group.save((err, group) => {
-        if (err) {
-          res.status(500);
-          res.json({
-            message: "Internal error occured"
-          });
-        } else {
-          res.status(201);
-          res.json(group);
-          console.log("Group successfully created");
-        }
+        
       });
+
+      /**
+       * Check if members contains a duplicated email.
+       * If there's more than 1 occurence of that email, you can't create a group.
+       */
+      for (let i = 0; i < members.length; i++) {
+        if (
+          members.filter((member) => {
+            return members[i].email === member.email;
+          }).length > 1
+        ) {
+          emailDoublons.push(members[i].email);
+        }
+      }
+
+      console.log("emailDoublon :>> ", emailDoublons);
+
+      if (emailDoublons.length === 0) {
+        const new_group = new Group({ ...req.body });
+
+        new_group.save((err, group) => {
+          if (err) {
+            res.status(500);
+            res.json({
+              message: "Internal error occured",
+            });
+          } else {
+            res.status(201);
+            res.json(group);
+            console.log("Group successfully created");
+          }
+        });
+      }
+      else{
+        res.status(500);
+      res.json({
+        // message: err.message, log error precis pour debug
+        message: "Internal error occured",
+      });
+      }
     } else {
       res.status(500);
       res.json({
         // message: err.message, log error precis pour debug
-        message: "Internal error occured"
+        message: "Internal error occured",
       });
     }
   } catch (err) {
+    res.status(500);
     res.json({
-      message: "Internal error occured"
+      message: "Internal error occured",
     });
     console.log(err);
   }
--- src/api/utils/utils.js
+++ src/api/utils/utils.js
@@ -83,3 +83,7 @@ exports.capitalize = (str) => {
     const splicedStr = str.slice(1, str.length).toLowerCase();
     return firstCharCapitalize + splicedStr;
 }
+
+/* exports.checkIfMembersExists = (arrMembers) => {
+
+} */
