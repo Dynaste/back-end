8b42068d2091168692acce8159fed00ff2034057
karzQ <karczinski.quentin@gmail.com>
2020-11-27 10:08:18 +0100

    Check if school already exist (by his name and location)

--- src/api/controllers/schoolController.js
+++ src/api/controllers/schoolController.js
@@ -1,4 +1,5 @@
 const School = require("../models/schoolModel");
+const { capitalize } = require("../utils/utils");
 
 // #region Schools
 
@@ -28,25 +29,38 @@ exports.list_all_schools = (req, res) => {
  * @param {*} res The response of the request.
  */
 exports.create_a_school = async (req, res) => {
-  if (req.body.name !== req.body.location && (req.body.name && req.body.location)) {
+  try {
+    if ((req.body.name && req.body.location) && req.body.name !== req.body.location) {
 
-    const new_school = new School({...req.body});
-    new_school.save((err, school) => {
-      if (err) {
-        res.status(500);
-        res.json({
-          message: "Server internal error.",
-        });
-      } else {
-        res.status(201);
-        res.json(school);
-        console.log("School successfully created");
+      // Equivalent to : SELECT * FROM School WHERE name = req.body.name AND location = req.body.location
+      let schoolInDb = await School.find({name: req.body.name, location: req.body.location}).exec();
+      console.log('School in db: ', schoolInDb);
+
+      if(schoolInDb && schoolInDb.length > 0){
+        throw "This school already exists."
       }
-    });
-  } else {
+      else{
+        
+        const new_school = new School({...req.body});
+
+        new_school.save((err, school) => {
+          if (err) {
+            throw "Server internal error"
+          } else {
+            res.status(201);
+            res.json(school);
+            console.log("School successfully created");
+          }
+        });    
+      }
+    }
+    else {
+      throw "You can't set the same value for name and location, all fields must be complete.";
+    }
+  } catch(err) {
     res.status(500);
     res.json({
-      message: "You can't set the same value for name and location."
+      message: err,
     });
   }
 } 
