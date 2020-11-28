1d55bd73357b1f8603f7102d4e1826483c312396
Dynaste <maximemary.pro@gmail.com>
2020-11-28 19:24:02 +0100

    get questions debug

--- src/api/controllers/questionsController.js
+++ src/api/controllers/questionsController.js
@@ -1,5 +1,12 @@
 exports.list_all_questions = (req, res) => {
-    const {questions} = require('../ressources/questions.json');
+    const questions = {
+        q1: "Quel est le nom du projet ? : ",
+        q2: "Quels sont les technologies utilisées pour ce projet ? : ",
+        q3: "Quel secteur visez vous ? (ex: Finance, Data, Medical) : ",
+        q4: "Parlez nous de votre solution : ",
+        q5: "Qu'attendez vous de ce Hackathon ? : "
+    
+    };
 
     res.status(200);
     res.json(questions);
